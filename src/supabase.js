f55cb89499e081984c4a5d048534b7a171b9133c
Cale <cale@gmail.com>
2026-05-02 16:26:26 +0200

    fix supabase integration

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://ijkjttiplxdkzbfhcorl.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlqa2p0dGlwbHhka3piZmhjb3JsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MTk5NTcsImV4cCI6MjA5MzI5NTk1N30.QJNqF26qYtqZXW5MOiHusONOGOCyTG1jIlBoXe0lKiY"
-);
